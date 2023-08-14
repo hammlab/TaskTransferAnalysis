e50f04ab627a9562dd54ff829f8ac0241badca1d
Akshay Mehra <akshaymehra2404@gmail.com>
2023-08-15 00:18:04 +0530

    Update transfer.py

--- large_scale/Other_Models/transfer.py
+++ large_scale/Other_Models/transfer.py
@@ -233,7 +233,7 @@ for epoch in range(2001):
             only_inputs=True,
         )[0]
         test_gradients = test_gradients.view(test_gradients.size(0), -1)
-        gradient_norm = torch.max(gradients.norm(2, dim=1))
+        gradient_norm = torch.max(test_gradients.norm(2, dim=1))
         print("Max grad norm:", gradient_norm.item())
         print("\n")
 
